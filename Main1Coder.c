f59e28ab65be4d2912d45592d9369428d51b1dcd
David Jacob <daviddjacob11@gmail.com>
2026-01-31 20:48:52 -0500

    A1 third draft

--- Main1Coder.c
+++ Main1Coder.c
@@ -59,6 +59,7 @@
 
 #define _CRT_SECURE_NO_WARNINGS 
 
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -103,16 +104,20 @@
 ************************************************************
 */
 
-// Main function to handle command-line arguments
+/* Main function to handle command - line arguments */
 urizen_int main1Coder(urizen_int argc, urizen_str* argv) {
-	if (argc < 5) {
-		printf("Usage: %s [cypher=1|decypher=0] <input_file> <output_file>\n", argv[0]);
-		return EXIT_FAILURE;
-	}
+
 	urizen_str operation = "";
 	urizen_str inputFileName = "";
 	urizen_str key = STR_LANGNAME;
 	urizen_str outputFileName = "";
+	clock_t start = 0;
+	clock_t end = 0;
+
+	if (argc < 5) {
+		printf("Usage: %s [cypher=1|decypher=0] <input_file> <output_file>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
 
 	if (argc > 4) {
 		urizen_int size;
@@ -123,7 +128,10 @@ urizen_int main1Coder(urizen_int argc, urizen_str* argv) {
 		inputFileName = argv[3]; /* input file name on the 4th row. */
 		outputFileName = argv[4]; /* output file name on the 5th row. */
 
-		// Call the appropriate function to file
+		/* Start timing how long encryption/decryption takes. */
+		start = clock();
+
+		/* Call the appropriate function to file */
 		if (atoi(operation) == CYPHER) /* converts the string "1" entered in the cmd line to an integer to cmpr to CYPHER. */
 			cypher(inputFileName, outputFileName, key); /* If the user entered 1, encrypt the input file. */
 		else if (atoi(operation) == DECYPHER)
@@ -133,9 +141,12 @@ urizen_int main1Coder(urizen_int argc, urizen_str* argv) {
 			return EXIT_FAILURE;
 		}
 
-		printf("Operation '%s' completed successfully.\n", operation);
+		/* Save end clock cycle.*/
+		end = clock();
 
-		// Call the other operation in memory
+		printf("Operation '%s' completed successfully in %.6f seconds.\n\n", operation, (double)(end - start) / CLOCKS_PER_SEC);
+
+		/* Call the other operation in memory */
 		size = (urizen_int)getSizeOfFile(outputFileName);
 
 		if (atoi(operation) == CYPHER)
@@ -147,6 +158,7 @@ urizen_int main1Coder(urizen_int argc, urizen_str* argv) {
 			for ( i = 0; i < size; i++) {
 				printf("%c", output[i]);
 			}
+			printf("\n");
 			free(output);
 		}
 		
--- Step1Coder.c
+++ Step1Coder.c
@@ -65,12 +65,11 @@
 #endif
 
 
-// Function to perform the Vigenère cipher (encoding or decoding)
+/* Function to perform the Vigenère cipher(encoding or decoding) */
 urizen_void vigenereFile(const urizen_str inputFileName, const urizen_str outputFileName, const urizen_str key, urizen_int encode) {
 
-	/* output file pointers. */
+	/* Declare variable at the top of the block. */
 	FILE* outputFile;
-
 	urizen_str output;
 	urizen_size size;
 
@@ -84,26 +83,33 @@ urizen_void vigenereFile(const urizen_str inputFileName, const urizen_str output
 		return;
 	}
 
+	/* Set the output to the value of the returned string post encoding/decoding. */
 	output = vigenereMem(inputFileName, key, encode ? CYPHER : DECYPHER);
 
+	/* Validate the output. */
 	if (!output) {
 		fclose(outputFile);
 		return;
 	}
-
+	/* retrieve the size of the file. */
 	size = getSizeOfFile(inputFileName);
+
+	/* Use the size to write the encoding/decoding to the user-specified file. */
 	fwrite(output, sizeof(urizen_char), size, outputFile);
+
+	/* Free the file data store in memory. */
 	free(output);
 
 	fclose(outputFile);
 }
 
-// Function to perform the Vigenère cipher (encoding or decoding)
+/* Function to perform the Vigenère cipher (encoding or decoding) */
 urizen_str vigenereMem(const urizen_str inputFileName, const urizen_str key, urizen_int encode) {
-
+	/* Declare variable at the top of the block. */
 	FILE* inputFile;
 	urizen_int size;
 	urizen_str output;
+
 
 	/* Check key first (cheap check) */
 	if (!key || strlen(key) == 0) {
@@ -114,6 +120,7 @@ urizen_str vigenereMem(const urizen_str inputFileName, const urizen_str key, uri
 	/* Get file size */
 	size = (urizen_int)getSizeOfFile(inputFileName);
 
+	/* Ensure the file isn't empty. */
 	if (size == 0) {
 		printf("Warning: File %s is empty.\n", inputFileName);
 		return NULL;
@@ -141,10 +148,12 @@ urizen_str vigenereMem(const urizen_str inputFileName, const urizen_str key, uri
 		fclose(inputFile);
 		return NULL;
 	}
-
+	/* Set null terminator so that that function such as printf know when the string ends. */
 	output[size] = '\0';
+	/* Close the file. */
 	fclose(inputFile);
 
+	/* Set the output to the value of the returned string post encoding/decoding. */
 	output = vigenereImpl(output, key, encode, size);
 
 	return output;
@@ -152,34 +161,45 @@ urizen_str vigenereMem(const urizen_str inputFileName, const urizen_str key, uri
 
 /* Implements the vigenere cypher. */
 urizen_str vigenereImpl(urizen_str output,const urizen_str key,urizen_int encode, urizen_int size) {
+	/* Declare variable at the top of the block. */
 	urizen_char offset;
 	urizen_int i = 0;
 	urizen_int j = 0;
 	urizen_int keyLen = (urizen_int)strlen(key);
 
+	/* Loop while the character index @i hasn't reached the file size @size. */
 	while (i < size) {
-
+		/* If key index @j has reached the end of the key's length (past the final character) then set it to 0. */
 		if (j == keyLen) {
 			j = 0;
 		}
-
+		/* Encrypt all characters besides new lines and carriage returns. */
 		if (output[i] != '\n' && output[i] != '\r' ) {
+			/* Calculate the offset by subtracting the value of the first visible ASCII character's index @ASCII_START from the key's index. */
 			offset = key[j] - ASCII_START + 1;
 			if (encode) {
+				/* If its an encoding, calculate the sum of the current character @output[i] and the offset @offset. */
 				urizen_int sum = output[i] + offset;
+
+				/* If the sum exceeds the range of the visible ASCII characters @ASCII_RANGE, then subtract the range's value from the sum to fix disproportionality. */
 				if (sum > ASCII_END) {
 					output[i] = (urizen_char)(sum - ASCII_RANGE);
 				}
 				else {
+					/* Otherwise set the character to the character value of the sum. */
 					output[i] = (urizen_char)sum;
 				}
 			}
 			else {
+				/* If its a decoding, calculate the difference between the current character @output[i] and the offset @offset. */
 				urizen_int diff = output[i] - offset;
+
+				/* If the difference precedes the start of the visible ASCII characters @ASCII_START, then add the range's value to the sum to fix disproportionality. */
 				if (diff < ASCII_START) {
 					output[i] = (urizen_char)(diff + ASCII_RANGE);
 				}
 				else {
+					/* Otherwise set the character to the character value of the difference. */
 					output[i] = (urizen_char)diff;
 				}
 			}
@@ -193,40 +213,47 @@ urizen_str vigenereImpl(urizen_str output,const urizen_str key,urizen_int encode
 
 }
 
-// Function to encode (cypher)
+/* Function to encode(cypher) */
 urizen_void cypher(const urizen_str inputFileName, const urizen_str outputFileName, const urizen_str key) {
     vigenereFile(inputFileName, outputFileName, key, CYPHER); /* vigFile controller selects */
 }
 
-// Function to decode (decypher)
+/* Function to decode(decypher) */
 urizen_void decypher(const urizen_str inputFileName, const urizen_str outputFileName, const urizen_str key) {
     vigenereFile(inputFileName, outputFileName, key, DECYPHER);
 }
 
-// Computes the file size (util method)
+/* Computes the file size(util method) */
 urizen_size getSizeOfFile(const urizen_str filename) {
-	urizen_int size = 0;
+	/* Declare variable at the top of the block. */
+	urizen_long size = 0;
 	FILE* inputFile;
 
+	/* Open the input file. */
 	inputFile = fopen(filename, "rb");
 
+	/* Validate the input file. */
 	if (!inputFile) {
 		printf("ERROR: The input file %s could not be reached.\n", filename);
 		return 0;
 	}
 
+	/* Move the file pointer to EOF. */
 	if (fseek(inputFile, 0, SEEK_END)) {
 		printf("ERROR: fseek operation failed for %s\n", filename);
 		fclose(inputFile);
 		return 0;
 	}
+	/* Assign byte-size to size. */
 	size = ftell(inputFile);
 
+	/* Validate size value. */
 	if (size < 0) {
+		fclose(inputFile);
 		return 0;
 	}
-
+	/* Close the input file. */
 	fclose(inputFile);
 
-    return size;
+    return (urizen_size)size;
 }
